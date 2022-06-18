2f630e1a796804597b8edc30f30c1adf9ff0db86
aykutklz <aykutkalaz@hotmail.com>
2022-06-18 03:41:58 +0300

    Servis arayüzleri ve Manager sınıfları oluşturuldu.

---
+++ Northwind.Business/Concrete/ProductManager.cs
@@ -0,0 +1,43 @@
+﻿using Northwind.Business.Abstract;
+using Northwind.DataAccess.Abstract;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            productDal = _productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetByCategory(int categoryId)
+        {
+            return _productDal.GetAll(p => p.CategoryId == categoryId);
+        }
+        public void Add(Product product)
+        {
+            _productDal.Add(product);
+        }
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
+        }
+
+        public void Delete(int productId)
+        {
+            _productDal.Delete(productId);
+        }
+    }
+}
