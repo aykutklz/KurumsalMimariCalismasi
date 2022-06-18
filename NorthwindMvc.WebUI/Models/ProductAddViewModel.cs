68e22b8da35cda4b1b540d685cdc62d803f2ab16
aykutklz <aykutkalaz@hotmail.com>
2022-06-18 12:43:17 +0300

    Admin'in ürünleri silmesi, güncellemesi, eklemesi gibi işlemlerin eklendi. Gerekli görünüm sayfaları eklendi.

---
+++ NorthwindMvc.WebUI/Models/ProductAddViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Northwind.Entities.Concrete;
+
+namespace NorthwindMvc.WebUI.Models
+{
+    public class ProductAddViewModel
+    {
+        public Product Product { get; set; }
+        public List<Category> Categories { get;  set; }
+    }
+}
---
+++ NorthwindMvc.WebUI/Models/ProductUpdateViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Northwind.Entities.Concrete;
+
+namespace NorthwindMvc.WebUI.Models
+{
+    public class ProductUpdateViewModel
+    {
+        public Product Product { get;  set; }
+        public List<Category> Categories { get;  set; }
+    }
+}
